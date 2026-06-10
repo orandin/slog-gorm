94ca8bc90b8fd3be1e5f441b9dd7bb1d245a35a2
tiandi <my.english.mail@qq.com>
2026-06-10 20:35:15 +0800

    trim sql for copy

--- logger.go
+++ logger.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log/slog"
 	"runtime"
+	"strings"
 	"time"
 
 	"gorm.io/gorm"
@@ -153,7 +154,7 @@ func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (sql strin
 	switch {
 	case err != nil && (!errors.Is(err, gorm.ErrRecordNotFound) || !l.ignoreRecordNotFoundError):
 		sql, rows := fc()
-
+		sql = l.trimSql(sql)
 		// Append context attributes
 		attributes := l.appendContextAttributes(ctx, []any{
 			slog.Any(l.errorField, err),
@@ -167,6 +168,7 @@ func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (sql strin
 
 	case l.slowThreshold != 0 && elapsed > l.slowThreshold:
 		sql, rows := fc()
+		sql = l.trimSql(sql)
 
 		// Append context attributes
 		attributes := l.appendContextAttributes(ctx, []any{
@@ -180,6 +182,7 @@ func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (sql strin
 
 	case l.traceAll || l.gormLevel == gormlogger.Info:
 		sql, rows := fc()
+		sql = l.trimSql(sql)
 
 		// Append context attributes
 		attributes := l.appendContextAttributes(ctx, []any{
@@ -191,6 +194,14 @@ func (l logger) Trace(ctx context.Context, begin time.Time, fc func() (sql strin
 
 		l.logAttrs(ctx, l.logLevel[DefaultLogType], fmt.Sprintf("SQL query executed [%s]", elapsed), attributes...)
 	}
+}
+
+func (l logger) trimSql(str string) string {
+	str = strings.Trim(str, " \n")
+	str = strings.Trim(str, " \t")
+	str = strings.Trim(str, " \r")
+	str = strings.Trim(str, " \\")
+	return str
 }
 
 func (l logger) appendContextAttributes(ctx context.Context, args []any) []any {
